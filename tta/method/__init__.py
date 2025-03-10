6007cfe4ff71f45c644d7816dd2766de99a2788a
Mos-start2092 <lasb0305@gmail.com>
2025-03-10 21:14:53 +0900

    SHOT

--- tta/method/__init__.py
+++ tta/method/__init__.py
@@ -3,6 +3,7 @@ from .source import Source
 from .tent import Tent
 from .lame import Lame
 from .memo import MEMO
+from .shot import SHOT
 
 
-__all__ = ["BaseMethod", "Source", "Tent", "Lame", "MEMO"]
+__all__ = ["BaseMethod", "Source", "Tent", "Lame", "MEMO","SHOT"]
